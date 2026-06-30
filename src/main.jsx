a3271c9273180a20d4f7ad04de04473bf991273b
nadia24021986-ship-it <nadia24021986@gmail.com>
2026-07-01 01:22:11 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import ReactDom from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-ReactDom.createRoot(document.getElementById('root')).render(
+// Menggunakan createRoot secara langsung untuk menghindari error impor default
+createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
